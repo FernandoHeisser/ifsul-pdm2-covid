3ae2f8b597f8c49cf2b371f48075f75e0c0ebd95
Fernando Heisser <fernando_heisser@dellteam.com>
2021-07-15 17:09:13 -0300

    Eighth commit

--- app/src/main/java/com/example/exercicio/ui/HistoryCovidCountry/HistoryCovidCountryARVH.java
+++ app/src/main/java/com/example/exercicio/ui/HistoryCovidCountry/HistoryCovidCountryARVH.java
@@ -9,30 +9,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.exercicio.R;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Headers;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
 
 public class HistoryCovidCountryARVH extends RecyclerView.Adapter<HistoryCovidCountryRVH> {
 
-    private final OkHttpClient client = new OkHttpClient();
     private final List<HistoryCovidCountryDTO> historyCovidCountryDTOList;
 
-    public HistoryCovidCountryARVH() {
-        run();
-
-        HistoryCovidCountryDTO historyCovidCountryDTO = new HistoryCovidCountryDTO();
+    public HistoryCovidCountryARVH() {HistoryCovidCountryDTO historyCovidCountryDTO = new HistoryCovidCountryDTO();
         historyCovidCountryDTO.date = "date";
         historyCovidCountryDTO.states = "states";
         historyCovidCountryDTO.positive = "positive";
@@ -72,30 +56,5 @@ public class HistoryCovidCountryARVH extends RecyclerView.Adapter<HistoryCovidCo
     @Override
     public int getItemCount() {
         return historyCovidCountryDTOList.size();
-    }
-
-    public void run() {
-        Request request = new Request.Builder()
-                .url("https://api.covidtracking.com/v1/us/daily.json")
-                .build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                try (ResponseBody responseBody = response.body()) {
-                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-
-                    Headers responseHeaders = response.headers();
-                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
-                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
-                    }
-
-                    System.out.println(Objects.requireNonNull(responseBody).string());
-                }
-            }
-        });
     }
 }
--- app/src/main/java/com/example/exercicio/ui/HistoryCovidCountry/HistoryCovidCountryFragment.java
+++ app/src/main/java/com/example/exercicio/ui/HistoryCovidCountry/HistoryCovidCountryFragment.java
@@ -12,7 +12,47 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.exercicio.R;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 public class HistoryCovidCountryFragment extends Fragment {
+
+    private final OkHttpClient client = new OkHttpClient();
+
+    public void getHistoryCovidCountryFromAPI() {
+        Request request = new Request.Builder()
+                .url("https://api.covidtracking.com/v1/us/daily.json")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                    Headers responseHeaders = response.headers();
+                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                    }
+
+                    System.out.println(Objects.requireNonNull(responseBody).string());
+                }
+            }
+        });
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
