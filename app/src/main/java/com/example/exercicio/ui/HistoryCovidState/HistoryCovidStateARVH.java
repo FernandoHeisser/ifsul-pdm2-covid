31c0db0c1d1010fe356c788860f240bb2082ac3c
Fernando Heisser <fernando_heisser@dellteam.com>
2021-07-15 19:53:30 -0300

    Eleventh commit

--- app/src/main/java/com/example/exercicio/ui/HistoryCovidState/HistoryCovidStateARVH.java
+++ app/src/main/java/com/example/exercicio/ui/HistoryCovidState/HistoryCovidStateARVH.java
@@ -1,10 +1,12 @@
 package com.example.exercicio.ui.HistoryCovidState;
 
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.exercicio.R;
@@ -16,24 +18,29 @@ public class HistoryCovidStateARVH extends RecyclerView.Adapter<HistoryCovidStat
 
     private final List<HistoryCovidStateDTO> historyCovidStateDTOList;
 
+    @RequiresApi(api = Build.VERSION_CODES.R)
     public HistoryCovidStateARVH(List<HistoryCovidState> historyCovidStateList) {
         List<HistoryCovidStateDTO> historyCovidStateDTOList = new ArrayList<>();
 
-        for (HistoryCovidState historyCovidState : historyCovidStateList) {
-            HistoryCovidStateDTO historyCovidStateDTO = new HistoryCovidStateDTO();
-            historyCovidStateDTO.date = historyCovidState.date;
-            historyCovidStateDTO.state = historyCovidState.state;
-            historyCovidStateDTO.positive = historyCovidState.positive;
-            historyCovidStateDTO.probableCases = historyCovidState.probableCases;
-            historyCovidStateDTO.negative = historyCovidState.negative;
-            historyCovidStateDTO.totalTestResults = historyCovidState.totalTestResults;
-            historyCovidStateDTO.hospitalizedCurrently = historyCovidState.hospitalizedCurrently;
-            historyCovidStateDTO.hospitalizedCumulative = historyCovidState.hospitalizedCumulative;
-            historyCovidStateDTO.recovered = historyCovidState.recovered;
-            historyCovidStateDTO.death = historyCovidState.death;
-            historyCovidStateDTOList.add(historyCovidStateDTO);
+        if (historyCovidStateList != null) {
+            for (HistoryCovidState historyCovidState : historyCovidStateList) {
+                HistoryCovidStateDTO historyCovidStateDTO = new HistoryCovidStateDTO();
+                historyCovidStateDTO.date = historyCovidState.date;
+                historyCovidStateDTO.state = historyCovidState.state;
+                historyCovidStateDTO.positive = historyCovidState.positive;
+                historyCovidStateDTO.probableCases = historyCovidState.probableCases;
+                historyCovidStateDTO.negative = historyCovidState.negative;
+                historyCovidStateDTO.totalTestResults = historyCovidState.totalTestResults;
+                historyCovidStateDTO.hospitalizedCurrently = historyCovidState.hospitalizedCurrently;
+                historyCovidStateDTO.hospitalizedCumulative = historyCovidState.hospitalizedCumulative;
+                historyCovidStateDTO.recovered = historyCovidState.recovered;
+                historyCovidStateDTO.death = historyCovidState.death;
+                historyCovidStateDTOList.add(historyCovidStateDTO);
+            }
+            this.historyCovidStateDTOList = historyCovidStateDTOList;
+        } else {
+            this.historyCovidStateDTOList = List.of(new HistoryCovidStateDTO());
         }
-        this.historyCovidStateDTOList = historyCovidStateDTOList;
     }
 
     @Override
